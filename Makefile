e845847b38037e523786b280c3e6cce66ce58657
Søren Brønsted <sbr@ufds.dk>
2013-07-24 15:37:05 +0200

    Fixed some cli arguments

--- Makefile
+++ Makefile
@@ -34,4 +34,4 @@ coverage:
 	phpunit --coverage-html doc/coverage test
 
 depend:
-	bin/depend.sh update
+	bin/depend.sh
--- bin/depend.sh
+++ bin/depend.sh
@@ -5,4 +5,4 @@ then
   curl -s https://getcomposer.org/installer | php -- --install-dir=bin
 fi
 
-bin/composer.phar -n update 
+bin/composer.phar --no-progress --no-interaction update
